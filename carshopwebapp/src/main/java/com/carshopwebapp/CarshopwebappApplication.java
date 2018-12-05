13cc3059b61430015791c56de2d034e4655ad464
AlexandreLeFou <alehunter@hotmail.gr>
2018-12-06 00:38:41 +0200

    spring intiializer, started project

---
+++ carshopwebapp/src/main/java/com/carshopwebapp/CarshopwebappApplication.java
@@ -0,0 +1,12 @@
+package com.carshopwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarshopwebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarshopwebappApplication.class, args);
+    }
+}
