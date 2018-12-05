d780d8eb115b50e1b96c4037e84ad32132bc940a
AlexandreLeFou <alehunter@hotmail.gr>
2018-12-06 01:07:33 +0200

    updated OwnerService with override methods-repository+changed OwnerRepository from crud to jpa (for list output)

--- carshopwebapp/src/main/java/com/carshopwebapp/Repository/OwnerRepository.java
+++ carshopwebapp/src/main/java/com/carshopwebapp/Repository/OwnerRepository.java
@@ -1,9 +1,8 @@
 package com.carshopwebapp.Repository;
 
 import com.carshopwebapp.entities.Owner;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OwnerRepository extends CrudRepository<Owner, Integer> {
+import org.springframework.data.jpa.repository.JpaRepository; //subrepository of Crud repository
+public interface OwnerRepository extends JpaRepository<Owner, Integer> {
     //be carefull for the owner to import the correct package
     //<..., Integer> due to the Location.class @id which is int ;)
 }
