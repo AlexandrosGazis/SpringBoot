947b18e32f27366f22c32b1c621b0f8a208485f4
AlexandreLeFou <alehunter@hotmail.gr>
2018-12-06 10:00:28 +0200

    created a user object,set all the fields and pass it from the controler->return it to the page

--- carshopwebapp/src/main/java/com/carshopwebapp/controllers/OwnerController.java
+++ carshopwebapp/src/main/java/com/carshopwebapp/controllers/OwnerController.java
@@ -1,15 +1,28 @@
 package com.carshopwebapp.controllers;
 
+import com.carshopwebapp.Service.OwnerService;
+import com.carshopwebapp.entities.Owner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
 
+    @Autowired
+    OwnerService service;
 
     @RequestMapping("/showCreate")
-    public String createNewOwner(){
+    public String createNewOwner() {
         return "createOwner";
     }
+
+    @RequestMapping("/saveOwn")
+    public String saveNewOwner(@ModelAttribute("owner") Owner owner) { //expose it out a as bean -spring container-
+        service.saveOwner(owner);
+        return "createOwner";//maprequest
+    }
+
 
 }
--- carshopwebapp/src/main/webapp/WEB-INF/views/createOwner.jsp
+++ carshopwebapp/src/main/webapp/WEB-INF/views/createOwner.jsp
@@ -9,7 +9,7 @@
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<form action="saveLoc" method="post">
+<form action="saveOwn" method="post">
       <pre>
     Id:   <input type="text" name="id"/>
 
