520248520e2145a340e85249488be86e3ad61cd5
AlexandreLeFou <alehunter@hotmail.gr>
2018-12-06 10:18:03 +0200

    added to the controller message for sucessfully ading data

--- carshopwebapp/src/main/java/com/carshopwebapp/controllers/OwnerController.java
+++ carshopwebapp/src/main/java/com/carshopwebapp/controllers/OwnerController.java
@@ -4,6 +4,8 @@ import com.carshopwebapp.Service.OwnerService;
 import com.carshopwebapp.entities.Owner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,10 +21,16 @@ public class OwnerController {
     }
 
     @RequestMapping("/saveOwn")
-    public String saveNewOwner(@ModelAttribute("owner") Owner owner) { //expose it out a as bean -spring container-
-        service.saveOwner(owner);
-        return "createOwner";//maprequest
+    public String saveNewOwner(@ModelAttribute("owner") Owner owner, ModelMap modelMap) { //expose it out a as bean -spring container-
+        Owner userSaved =  service.saveOwner(owner);
+        String msg = "Επιτυχης εισαγωγη δεδομενων με id: "+ userSaved.getId();
+        modelMap.addAttribute("msg",msg);
+        return "createOwner";
+        //request modelattribute
+        //response modelmap (pass key value pairs)
     }
+
+    
 
 
 }
--- carshopwebapp/src/main/webapp/WEB-INF/views/createOwner.jsp
+++ carshopwebapp/src/main/webapp/WEB-INF/views/createOwner.jsp
@@ -35,6 +35,11 @@
     <input type="submit" value="Αποθηκευσε τα Δεδομενα"/>
     </pre>
 </form>
+${msg}
+
+<a href="displayOwners">View All Data!</a>
+
+
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
