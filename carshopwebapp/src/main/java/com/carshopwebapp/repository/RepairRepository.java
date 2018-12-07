3e2f799ef580360293f5b4a193826af018652434
AlexandreLeFou <alehunter@hotmail.gr>
2018-12-07 15:50:31 +0200

    added RepairRepository ie extend jpa repository

---
+++ carshopwebapp/src/main/java/com/carshopwebapp/repository/RepairRepository.java
@@ -0,0 +1,10 @@
+package com.carshopwebapp.repository;
+
+import com.carshopwebapp.entities.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface RepairRepository extends JpaRepository<Owner, Integer> {
+    //be carefull for the owner to import the correct package
+    //<..., Integer> due to the Location.class @id which is int ;)
+}
