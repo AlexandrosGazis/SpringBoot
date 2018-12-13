f0bec17a04126ee7c3b067f407f9face364ef9f8
AlexandreLeFou <alehunter@hotmail.gr>
2018-12-14 00:11:52 +0200

    added login redirect

--- carshopwebappfinal/src/main/java/com/carshopwebapp/config/UsersLoginSecurityConfiguration.java
+++ carshopwebappfinal/src/main/java/com/carshopwebapp/config/UsersLoginSecurityConfiguration.java
@@ -1,18 +1,21 @@
 package com.carshopwebapp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class UsersLoginSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    
+    @Autowired
+    LoginSecuritySimpleAuthenticationSuccessHandler loginSecuritySimpleAuthenticationSuccessHandler;
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -22,26 +25,49 @@ public class UsersLoginSecurityConfiguration extends WebSecurityConfigurerAdapte
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/welcome")
-            .hasAnyRole("USER", "ADMIN").antMatchers("/showCreate")
-            .hasAnyRole("USER", "ADMIN").antMatchers("/saveOwn")
-            .hasAnyRole("USER", "ADMIN").antMatchers("/showCreateRepair")
-            .hasAnyRole("USER", "ADMIN").antMatchers("/saveRepair")
-            .hasAnyRole("ADMIN").anyRequest().authenticated().and().formLogin()
-            .permitAll().and().logout().permitAll();
+                .hasAnyRole("USER", "ADMIN").antMatchers("/showCreate")
+                .hasAnyRole("USER", "ADMIN").antMatchers("/saveOwn")
+                .hasAnyRole("USER", "ADMIN").antMatchers("/showCreateRepair")
+                .hasAnyRole("ADMIN").antMatchers("/saveRepair")
+                .hasAnyRole("ADMIN").anyRequest().authenticated().and().formLogin()
+                .successHandler(loginSecuritySimpleAuthenticationSuccessHandler)
+                .permitAll().and().logout().permitAll();
 
         http.csrf().disable();
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder authenticationMgr) throws Exception {
-        authenticationMgr.inMemoryAuthentication()
-           .withUser("superuser").password("superuser").authorities("ROLE_USER")
-           .and()
-           .withUser("user").password("user").authorities("ROLE_USER", "ROLE_ADMIN");
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        authenticationMgr.inMemoryAuthentication().passwordEncoder(encoder)
+                .withUser("user").password(encoder.encode("user")).authorities("ROLE_USER")
+                .and()
+                .withUser("superuser").password(encoder.encode("superuser")).authorities("ROLE_USER", "ROLE_ADMIN");
     }
 
+
+ /*   @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+*/
+}
+/*
+
+    @SuppressWarnings("deprecation")
     @Bean
     public static NoOpPasswordEncoder passwordEncoder() {
         return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
     }
-}
+
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+    */
+
+
+
+
--- carshopwebappfinal/src/main/java/com/carshopwebapp/controllers/LoginController.java
+++ carshopwebappfinal/src/main/java/com/carshopwebapp/controllers/LoginController.java
@@ -3,19 +3,22 @@ package com.carshopwebapp.controllers;
 
 import com.carshopwebapp.services.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Set;
 
 @Controller
 
-public class LoginController {
+public class LoginController implements AuthenticationSuccessHandler {
 
     @Autowired
     OwnerService service;
@@ -25,7 +28,32 @@ public class LoginController {
     public ModelAndView firstPage() {
         return new ModelAndView("welcome");
     }
+/*
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginPage(HttpServletRequest httpServletRequest, Model model) {
+        if(httpServletRequest.isUserInRole("ROLE_ADMIN")) {
+            return "redirect:/index2.html";
+        } else if(httpServletRequest.isUserInRole("ROLE_USER")) {
+            return "redirect:/sintelestes.html";
+        } else {
+            return "";
+        }
+    }
 
 
+*/
 
-}
+        @Override
+        public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+
+            Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+
+            if (roles.contains("ROLE_ADMIN")) {
+                httpServletResponse.sendRedirect("/welcome");
+            } else {
+                httpServletResponse.sendRedirect("/");
+            }
+        }
+    }
+
+
