2ec106bcf66dcd62cb75b78c59a13c85893752af
AlexandreLeFou <alehunter@hotmail.gr>
2018-12-14 17:02:23 +0200

    changed contributors page

--- carshopwebappfinal/src/main/java/com/carshopwebapp/controllers/GenericLinksController.java
+++ carshopwebappfinal/src/main/java/com/carshopwebapp/controllers/GenericLinksController.java
@@ -1,15 +1,10 @@
 package com.carshopwebapp.controllers;
 
 
-import com.carshopwebapp.entitities.Owner;
 import com.carshopwebapp.services.OwnerService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -23,7 +18,7 @@ public class GenericLinksController {
 
     @RequestMapping("/")
     public String startPage() {
-        return "redirect:/index2.html";
+        return "redirect:/index2";
     }
 
     @RequestMapping("/sintelestes")
